5b73946545b61d96d5bb3175bd820acf6e72575c
Wolf Vollprecht <w.vollprecht@gmail.com>
2013-10-30 20:39:03 +0100

    reworked whole project, for now without a QT frontend

---
+++ CMakeFiles/Pathfinder.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Pathfinder.dir/main.cpp.o"
+  "Pathfinder.pdb"
+  "Pathfinder"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Pathfinder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
