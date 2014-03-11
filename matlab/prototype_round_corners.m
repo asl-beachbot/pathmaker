1ecd9082084a22c69bee972aeba55c86250dc2a9
Wolf Vollprecht <w.vollprecht@gmail.com>
2014-03-12 00:30:03 +0100

    working on

--- matlab/prototype_round_corners.m
+++ matlab/prototype_round_corners.m
@@ -1,6 +1,7 @@
-p1 = [0, 0]
+clf;
+p1 = [4, 0]
 p2 = [1, 1]
-p3 = [0, 4]
+p3 = [10, 1]
 
 rotation_p90 = [cos(pi/2)  -sin(pi/2) ; sin(pi/2)  cos(pi/2)]
 rotation_m90 = [cos(-pi/2)  -sin(-pi/2) ; sin(-pi/2)  cos(-pi/2)]
@@ -15,11 +16,13 @@ angle = acos(dot(v1n, v2n))
 
 deg_angle = angle / pi * 180
 
+l_down = tan(angle / 2) * 0.5;
 
 
-v1_m = p2 - (v1n*0.5) - (v1n*0.5*rotation_p90);
+v1_m = p2 - (v1n*0.5) - (v1n*l_down*rotation_p90);
 
 hold on
+axis equal
 plot([p1(1), p2(1), p3(1)]', [p1(2), p2(2), p3(2)]')
 pv = [p2 - (v1n*0.5); v1_m; p2 - (v2n*0.5)];
 plot(pv(:,1), pv(:,2), '-oc');
